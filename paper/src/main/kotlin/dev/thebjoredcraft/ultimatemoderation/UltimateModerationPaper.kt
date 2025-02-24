e2c9fcfc8e48572a6b14fa9c18ccd84d705c77f8
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-24 15:58:13 +0100

    feat: add send method

--- paper/src/main/kotlin/dev/thebjoredcraft/ultimatemoderation/UltimateModerationPaper.kt
+++ paper/src/main/kotlin/dev/thebjoredcraft/ultimatemoderation/UltimateModerationPaper.kt
@@ -1,6 +1,11 @@
 package dev.thebjoredcraft.ultimatemoderation
 
 import com.github.shynixn.mccoroutine.bukkit.SuspendingJavaPlugin
+import dev.thebjoredcraft.ultimatemoderation.util.Colors
+import dev.thebjoredcraft.ultimatemoderation.util.MessageBuilder
+import dev.thebjoredcraft.ultimatemoderation.util.PageableMessageBuilder
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin.getPlugin
 
 val plugin: UltimateModerationPaper get() = getPlugin(UltimateModerationPaper::class.java)
@@ -8,5 +13,11 @@ val plugin: UltimateModerationPaper get() = getPlugin(UltimateModerationPaper::c
 class UltimateModerationPaper(): SuspendingJavaPlugin() {
     override suspend fun onEnableAsync() {
 
+    }
+
+    companion object {
+        fun send(messageBuilder: MessageBuilder, vararg players: Player) {
+            players.forEach { player -> player.sendMessage(Colors.PREFIX.append(messageBuilder.build())) }
+        }
     }
 }
