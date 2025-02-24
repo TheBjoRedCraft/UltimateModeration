e517e64de764902ab2a0ca1cf5a506afc6afd68b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-24 16:45:15 +0100

    feat: add messgae

--- paper/src/main/kotlin/dev/thebjoredcraft/ultimatemoderation/spectatemode/SpectateModeCommand.kt
+++ paper/src/main/kotlin/dev/thebjoredcraft/ultimatemoderation/spectatemode/SpectateModeCommand.kt
@@ -2,6 +2,8 @@ package dev.thebjoredcraft.ultimatemoderation.spectatemode
 
 import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
+import dev.thebjoredcraft.ultimatemoderation.UltimateModerationPaper
+import dev.thebjoredcraft.ultimatemoderation.util.MessageBuilder
 
 class SpectateModeCommand(commandName: String): CommandAPICommand(commandName) {
     init {
@@ -9,6 +11,12 @@ class SpectateModeCommand(commandName: String): CommandAPICommand(commandName) {
         withSubcommands(SpectateModeListCommand("list"))
         executesPlayer(PlayerCommandExecutor() { player, args ->
             SpectateModeService.toggle(player)
+
+            if(SpectateModeService.isSpectating(player)) {
+                UltimateModerationPaper.send(MessageBuilder().primary("Du bist nun im ").success("Spectate-Modus"), player)
+            } else {
+                UltimateModerationPaper.send(MessageBuilder().primary("Du bist nun nicht mehr im ").error("Spectate-Modus"), player)
+            }
         })
     }
 }
